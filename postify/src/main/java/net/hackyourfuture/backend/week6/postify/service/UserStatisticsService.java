0e2d640a1fc4bb7b146532734bff420847a81fcf
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-06-09 13:27:14 +0200

    Implement UserStatisticsService with proper repository wiring

--- postify/src/main/java/net/hackyourfuture/backend/week6/postify/service/UserStatisticsService.java
+++ postify/src/main/java/net/hackyourfuture/backend/week6/postify/service/UserStatisticsService.java
@@ -1,1 +1,18 @@
 package net.hackyourfuture.backend.week6.postify.service;
+
+import net.hackyourfuture.backend.week6.postify.model.UserStatisticsResponse;
+import net.hackyourfuture.backend.week6.postify.repository.UserStatisticsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserStatisticsService {
+
+    @Autowired
+    private  UserStatisticsRepository repo;
+
+    public UserStatisticsResponse getStats(int userId) {
+        return repo.getUserStats(userId);
+    }
+
+}
