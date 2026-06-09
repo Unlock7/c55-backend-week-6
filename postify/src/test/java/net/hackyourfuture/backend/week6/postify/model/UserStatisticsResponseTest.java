0f9d3408b06b11ed5f1d1142cfd6bc6e65eccf84
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-06-09 18:13:39 +0200

    Add unit test for UserStatisticsResponse fields

--- postify/src/test/java/net/hackyourfuture/backend/week6/postify/model/UserStatisticsResponseTest.java
+++ postify/src/test/java/net/hackyourfuture/backend/week6/postify/model/UserStatisticsResponseTest.java
@@ -1,4 +1,21 @@
 package net.hackyourfuture.backend.week6.postify.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserStatisticsResponseTest {
+
+    @Test
+    void shouldContainCorrectUserStatistics() {
+        UserStatisticsResponse response = new UserStatisticsResponse();
+
+        response.userId = 1;
+        response.userName = "Abraham";
+        response.userCountry = "CA";
+
+        assertEquals(1, response.userId);
+        assertEquals("Abraham", response.userName);
+        assertEquals("CA", response.userCountry);
+    }
 }
