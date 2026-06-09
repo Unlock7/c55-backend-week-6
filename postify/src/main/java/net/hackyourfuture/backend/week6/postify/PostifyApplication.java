0852d49d50e699d254ac508c55f41e5ae0ee139e
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-06-09 15:19:29 +0200

    Use Dotenv to inject DB_USERNAME and DB_PASSWORD at startup

--- postify/src/main/java/net/hackyourfuture/backend/week6/postify/PostifyApplication.java
+++ postify/src/main/java/net/hackyourfuture/backend/week6/postify/PostifyApplication.java
@@ -1,5 +1,6 @@
 package net.hackyourfuture.backend.week6.postify;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PostifyApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+
 		SpringApplication.run(PostifyApplication.class, args);
 	}
 
