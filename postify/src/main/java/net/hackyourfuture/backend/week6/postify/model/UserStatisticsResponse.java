9508ba8972941fc006159f42cb99fdba94b257a3
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-06-09 15:22:19 +0200

    Add JsonPropertyOrder to enforce response field order

--- postify/src/main/java/net/hackyourfuture/backend/week6/postify/model/UserStatisticsResponse.java
+++ postify/src/main/java/net/hackyourfuture/backend/week6/postify/model/UserStatisticsResponse.java
@@ -1,12 +1,25 @@
 package net.hackyourfuture.backend.week6.postify.model;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({
+        "userId",
+        "userName",
+        "userCountry",
+        "totalStreams",
+        "uniqueTracksStreamed",
+        "uniqueArtistsStreamed",
+        "favoriteGenre",
+        "totalListeningTimeSeconds"
+})
+
 public class UserStatisticsResponse {
 
-        public int userID;
+        public int userId;
         public  String userName;
         public String userCountry;
         public int totalStreams;
-        public int uniqueTrackStreamed;
+        public int uniqueTracksStreamed;
         public int uniqueArtistsStreamed;
         public String favoriteGenre;
         public int totalListeningTimeSeconds;
