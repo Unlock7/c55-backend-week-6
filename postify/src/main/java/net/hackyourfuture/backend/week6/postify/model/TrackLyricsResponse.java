2eacbe04d62c3652a6b7d75125d4ff20dd88b541
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-06-10 13:00:46 +0200

    feat: add response model for track lyrics API

---
+++ postify/src/main/java/net/hackyourfuture/backend/week6/postify/model/TrackLyricsResponse.java
@@ -0,0 +1,19 @@
+package net.hackyourfuture.backend.week6.postify.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrackLyricsResponse {
+    private int trackId;
+    private String trackTitle;
+    private String artistName;
+    private String lyrics;
+
+}
