dfa31bd5c5af300caf83d921aa4140d8027c1894
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-06-10 19:36:53 +0200

    chore: add RestClient configuration class

--- postify/src/main/java/net/hackyourfuture/backend/week6/postify/config/RestClientConfig.java
+++ postify/src/main/java/net/hackyourfuture/backend/week6/postify/config/RestClientConfig.java
@@ -1,4 +1,16 @@
 package net.hackyourfuture.backend.week6.postify.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
 public class RestClientConfig {
+
+    @Bean
+    public RestClient lyricsRestClient() {
+        return RestClient.builder()
+                .baseUrl("https://api.lyrics.ovh/v1")
+                .build();
+    }
 }
