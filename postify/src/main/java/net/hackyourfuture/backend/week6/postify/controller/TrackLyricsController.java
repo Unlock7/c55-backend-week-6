634853e1c956950a859a646fbac8efb05f2a9fa9
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-06-10 19:39:05 +0200

    feat: implement TrackLyricsController for lyrics endpoint

--- postify/src/main/java/net/hackyourfuture/backend/week6/postify/controller/TrackLyricsController.java
+++ postify/src/main/java/net/hackyourfuture/backend/week6/postify/controller/TrackLyricsController.java
@@ -1,4 +1,26 @@
+
 package net.hackyourfuture.backend.week6.postify.controller;
 
+import net.hackyourfuture.backend.week6.postify.dto.TrackLyricsResponse;
+import net.hackyourfuture.backend.week6.postify.service.TrackLyricsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tracks")
 public class TrackLyricsController {
+
+    private final TrackLyricsService trackLyricsService;
+
+    public TrackLyricsController(
+            TrackLyricsService trackLyricsService) {
+
+        this.trackLyricsService = trackLyricsService;
+    }
+
+    @GetMapping("/{id}/lyrics")
+    public TrackLyricsResponse getLyrics(
+            @PathVariable Long id) {
+
+        return trackLyricsService.getLyrics(id);
+    }
 }
